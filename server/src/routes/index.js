438dea2897d8ea404538120ece59f33bef0d82dd
Kalyna-Serhii <kalyna.serhii@lll.kpi.ua>
2023-12-06 13:26:27 +0200

    Added reviews in the server

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,5 +1,6 @@
 import authRouter from "./auth-router.js";
 import userRouter from "./user-router.js";
 import serviceRouter from "./service-router.js";
+import reviewRouter from "./review-router.js";
 
-export {authRouter, userRouter, serviceRouter};
+export {authRouter, userRouter, serviceRouter, reviewRouter};
---
+++ server/src/services/review-service.js
@@ -0,0 +1,54 @@
+import ApiError from '../exceptions/api-error.js';
+import reviewModel from '../models/review-model.js';
+import tokenService from "./token-service.js";
+
+const ReviewService = {
+    async getReviews() {
+        const reviews = await reviewModel.findAll();
+        return reviews;
+    },
+
+    async getReviewById(id) {
+        const review = await reviewModel.findOne({where: {id}});
+        if (!review) {
+            throw ApiError.BadRequest('Review not found');
+        }
+        return review;
+    },
+
+    async createReview(token, body) {
+        const userData = tokenService.validateAccessToken(token);
+        if (!userData) {
+            throw ApiError.UnauthorizedError();
+        }
+        const userId = userData.id;
+        const {message} = body;
+        const today = new Date();
+        const date = today.toISOString().split('T')[0];
+        const newReview = await reviewModel.create({userId, message, date});
+        return newReview;
+    },
+
+    async updateReview(id, body) {
+        const review = await reviewModel.findOne({where: {id}});
+        if (!review) {
+            throw ApiError.BadRequest('Review not found');
+        }
+        const {message, date} = body;
+        const updatedFields = {};
+        updatedFields.message = message;
+        updatedFields.date = date;
+        const updatedReview = await review.update(updatedFields);
+        return updatedReview;
+    },
+
+    async deleteReview(id) {
+        const review = await reviewModel.findOne({where: {id}});
+        if (!review) {
+            throw ApiError.BadRequest('Review not found');
+        }
+        await review.destroy();
+    },
+};
+
+export default ReviewService;
