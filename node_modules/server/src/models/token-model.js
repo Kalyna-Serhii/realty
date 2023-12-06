8114badf104fe671372de85aface99853b381e6c
Kalyna-Serhii <kalyna.serhii@lll.kpi.ua>
2023-12-06 17:35:01 +0200

    Added CRUD of realty in the server

---
+++ node_modules/server/src/models/token-model.js
@@ -0,0 +1,24 @@
+import UserModel from './user-model.js';
+import {DataTypes, sequelize} from '../database/database.config.js';
+
+const TokenModel = sequelize.define(
+    'Token',
+    {
+        userId: {
+            type: DataTypes.INTEGER(),
+            allowNull: false,
+        },
+        refreshToken: {
+            type: DataTypes.TEXT(),
+            allowNull: false,
+        },
+    },
+    {
+        tableName: 'tokens',
+        timestamps: false,
+    },
+);
+
+TokenModel.belongsTo(UserModel, {foreignKey: 'userId'});
+
+export default TokenModel;
---
+++ node_modules/server/src/routes/service-router.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import serviceController from '../controllers/service-controller.js';
+
+const router = express.Router();
+
+router.get('/services', serviceController.getServices);
+router.get('/service/:id', serviceController.getServiceById);
+router.post('/service', serviceController.createService);
+router.patch('/service/:id', serviceController.updateService);
+router.delete('/service/:id', serviceController.deleteService);
+
+export default router;
