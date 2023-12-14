d1d820794042b39e101f7e344cd5489aebe3156c
Kalyna-Serhii <kalyna.serhii@lll.kpi.ua>
2023-12-14 13:57:11 +0200

    Fixed logout

--- client/src/components/LogoutComponent.vue
+++ client/src/components/LogoutComponent.vue
@@ -9,11 +9,9 @@ import {deleteFromLocalStorage} from "@/utils/LocalStorage";
 export default {
   methods: {
     async submitRequest() {
-      const response = await api.auth.logout();
-      if (response && response.status === 200) {
-        deleteFromLocalStorage();
-        this.$router.push('/');
-      }
+      await api.auth.logout();
+      deleteFromLocalStorage();
+      this.$router.push('/');
     },
   },
   async mounted() {
